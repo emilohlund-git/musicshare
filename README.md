204f7d57cdbf8a0a4decf77f023e287b45211c85
Emil <emil@emilohlund.se>
2020-01-28 13:13:32 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Music Project #
+
+Reasoning behind this project is that I really like music, and I feel like having a medium where you can share your favorite songs with ease would be very nice. 
+
+Background image from Pexels.com [https://www.pexels.com/@shottrotter]
---
+++ app.py
@@ -0,0 +1,28 @@
+import os
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+app = Flask(__name__, static_url_path='/static')
+
+app.config["MONGO_DBNAME"] = "MusicProject"
+app.config["MONGO_URI"] = "mongodb+srv://Emil:Hampussuger1234@cluster0-hhlkk.mongodb.net/musicproject?retryWrites=true&w=majority"
+
+mongo = PyMongo(app)
+
+@app.route('/')
+def home_page():
+    return render_template('index.html')
+
+@app.route('/addsong')
+def add_song():
+    return render_template('addsong.html')
+
+@app.route('/insertsong', methods=['POST'])
+def insert_song():
+    songs = mongo.db.songs
+    songs.insert_one(request.form.to_dict())
+    return redirect(url_for('home_page'))
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'), port=os.environ.get('PORT'), debug=True)
