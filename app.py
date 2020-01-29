a7fde0eb78425196d9bed51ce5d32700705a5c3c
Emil <emil@emilohlund.se>
2020-01-29 19:11:07 +0100

    Added a table which displays the information that is stored in the database in a structured manner. Also added functionality to register an account aswell as being redirected to an apprioriate page once you have registered. Plus a bit of design work.

--- app.py
+++ app.py
@@ -5,18 +5,23 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__, static_url_path='/static')
 
+password = os.getenv('MY_PASSWORD')
+
 app.config["MONGO_DBNAME"] = "MusicProject"
-app.config["MONGO_URI"] = "mongodb+srv://Emil:Hampussuger1234@cluster0-hhlkk.mongodb.net/musicproject?retryWrites=true&w=majority"
+app.config["MONGO_URI"] = "mongodb+srv://Emil:{}@cluster0-hhlkk.mongodb.net/musicproject?retryWrites=true&w=majority".format(password)
 
 mongo = PyMongo(app)
+
 
 @app.route('/')
 def home_page():
     return render_template('index.html')
 
+
 @app.route('/addsong')
 def add_song():
     return render_template('addsong.html')
+
 
 @app.route('/insertsong', methods=['POST'])
 def insert_song():
@@ -24,9 +29,30 @@ def insert_song():
     songs.insert_one(request.form.to_dict())
     return redirect(url_for('home_page'))
 
+
 @app.route('/tutorial')
 def tutorial():
     return render_template('tutorial.html')
+
+
+@app.route('/library')
+def library():
+    return render_template('library.html', songs=mongo.db.songs.find())
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
+@app.route('/adduser', methods=['POST'])
+def adduser():
+    new_user = request.form['username']
+    if mongo.db.users.find_one({"username": new_user}):
+        return render_template('wrong.html')
+    else:
+        return render_template('welcome.html')
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'), port=os.environ.get('PORT'), debug=True)
---
+++ static/js/tutorial.js
@@ -0,0 +1,96 @@
+document.getElementById("music-add").style.color = "orange";
+var current_page = 1;
+const next_button = document.getElementById("next-button");
+const prev_button = document.getElementById("prev-button");
+let title = document.getElementById("tutorial-title");
+let description = document.getElementById("tutorial-description");
+const breadcrumb_items = [
+  document.getElementById("1"),
+  document.getElementById("2"),
+  document.getElementById("3"),
+  document.getElementById("4")
+];
+
+next_button.addEventListener("click", function() {
+  if (current_page < 4) {
+    current_page++;
+  }
+  change_page();
+});
+
+prev_button.addEventListener("click", function() {
+  if (current_page > 1) {
+    current_page--;
+  }
+  change_page();
+});
+
+function change_page() {
+  switch (current_page) {
+    case 1:
+      document.getElementById("music-add").style.color = "orange";
+      document.getElementById("music-library").style.color = "white";
+      breadcrumb_items.forEach(function(item) {
+        item.classList.remove("active");
+      });
+      breadcrumb_items[0].classList.add("active");
+
+      title.innerHTML = current_page + ". Add a song to the library!";
+      description.innerHTML =
+        "By pressing this icon " +
+        '<i class="material-icons">library_add</i>' +
+        " it can be found in the navigation bar above.";
+
+      prev_button.classList.add("disabled");
+      prev_button.classList.remove("waves-effect");
+      break;
+    case 2:
+      document.getElementById("music-add").style.color = "white";
+      document.getElementById("music-library").style.color = "orange";
+      breadcrumb_items.forEach(function(item) {
+        item.classList.remove("active");
+      });
+      breadcrumb_items[1].classList.add("active");
+
+      title.innerHTML =
+        current_page + ". Look through others contributions to the library.";
+      description.innerHTML =
+        "You might find something new that you enjoy! Look for this symbol in the navigation bar " +
+        '<i class="material-icons">library_music</i>';
+
+      break;
+    case 3:
+      document.getElementById("music-library").style.color = "white";
+      breadcrumb_items.forEach(function(item) {
+        item.classList.remove("active");
+      });
+      breadcrumb_items[2].classList.add("active");
+
+      title.innerHTML = current_page + ". Search through all our music!";
+      description.innerHTML =
+        "You can use the search box above to find anything on the website.";
+
+      break;
+    case 4:
+      breadcrumb_items.forEach(function(item) {
+        item.classList.remove("active");
+      });
+      breadcrumb_items[3].classList.add("active");
+
+      title.innerHTML = current_page + ". Simple as that, now go try it out!";
+      description.innerHTML = "";
+
+      next_button.classList.add("disabled");
+      next_button.classList.remove("waves-effect");
+      break;
+  }
+
+  if (current_page < 4) {
+    next_button.classList.remove("disabled");
+    next_button.classList.add("waves-effect");
+  }
+  if (current_page > 1) {
+    prev_button.classList.remove("disabled");
+    prev_button.classList.add("waves-effect");
+  }
+}
